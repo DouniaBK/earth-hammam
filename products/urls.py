f329d3899ab86cc951aae8752697dd6cc91d0477
DouniaBK <douniazedbacha@gmail.com>
2023-10-19 12:32:56 +0000

    add: Treatments and Tickets template and view

--- products/urls.py
+++ products/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.all_items, name='items'),
-    path('<item_id>', views.item_detail, name='item_detail'),
+    #path('<item_id>', views.item_detail, name='item_detail'),
+    path('', views.all_treatments, name='treatments'),
+    path('', views.all_tickets, name='tickets'),
 ]
--- products/views.py
+++ products/views.py
@@ -10,14 +10,27 @@ def all_items(request):
     items = Item.objects.filter(category_id='3')
     return render(request, 'products/products.html', {'items': items})
 
-
+""" 
 def item_detail(request, item_id):
-    """ A view to show individual item details """
+   A view to show individual item details
 
     item = get_object_or_404(Item, pk=item_id)
-
     context = {
         'item': item,
     }
 
     return render(request, 'products/item_detail.html', context)
+
+"""
+def all_treatments(request):
+    """ A view to show all items in category treatments """
+
+    treatments = Item.objects.filter(category_id='1')
+    return render(request, 'products/treatments.html', {'treatments': treatments})
+
+
+def all_tickets(request):
+    """ A view to show all items in category tickets """
+
+    tickets = Item.objects.filter(category_id='2')
+    return render(request, 'products/tickets.html', {'tickets': tickets})
