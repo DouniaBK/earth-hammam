44c48f8ac1f970baf88e0186fd7902627f520a23
DouniaBK <douniazedbacha@gmail.com>
2023-10-24 08:44:16 +0000

    add: Shopping cart add functionality

--- products/views.py
+++ products/views.py
@@ -26,3 +26,15 @@ def all_tickets(request):
 
 def tickets(request):
     return render(request, 'tickets.html', {})
+
+
+def item_detail(request, product_id):
+    """ A view to show individual product details """
+
+    item = get_object_or_404(Item, pk=product_id)
+
+    context = {
+        'item': item,
+    }
+
+    return render(request, 'products/item_detail.html', context)
