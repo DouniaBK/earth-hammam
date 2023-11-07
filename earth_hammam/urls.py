ad3a33ae945904c8167215c59fea15ce218b21cc
DouniaBK <douniazedbacha@gmail.com>
2023-11-07 13:40:40 +0000

    fix: Cloudinary urls not working

--- earth_hammam/urls.py
+++ earth_hammam/urls.py
@@ -27,7 +27,7 @@ urlpatterns = [
     path('bag/', include('bag.urls')),
     path('checkout/', include('checkout.urls')),
     path('profile/', include('profiles.urls')),
-] # + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]  + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+#if settings.DEBUG:
+#   urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
