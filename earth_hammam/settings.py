bf84a76212346bf5ed2d93d59a32bd742fcded6d
DouniaBK <douniazedbacha@gmail.com>
2023-11-28 23:09:06 +0000

    fix: change debug call

--- earth_hammam/settings.py
+++ earth_hammam/settings.py
@@ -31,7 +31,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DEBUG', True)
+#DEBUG = os.environ.get('DEBUG', True)
+DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
 ALLOWED_HOSTS = ['*']
 
