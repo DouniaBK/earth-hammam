695c6ea44e07dc72be3f5e3aa95beaeaee581345
DouniaBK <douniazedbacha@gmail.com>
2023-12-11 20:53:27 +0000

    add: debug output

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -165,10 +165,13 @@ class StripeWH_Handler:
                 attempt += 1
                 time.sleep(1)
 
-        print('handle_payment_intent_succeeded order')
-        print(order)
         print('order_exists')
         print(order_exists)
+        print('handle_payment_intent_succeeded order')
+        try:
+            print(order)
+        except:
+            print('NO')
 
         if order_exists:
             self._send_confirmation_email(order)
