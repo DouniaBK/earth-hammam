db5aa39fd1fe257ffb9d441e7a4de058c7d6c204
DouniaBK <douniazedbacha@gmail.com>
2024-02-06 13:49:21 +0000

    Update: requirements.txt

--- earth_hammam/settings.py
+++ earth_hammam/settings.py
@@ -31,9 +31,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = ['*']
+DEBUG = True
+
+ALLOWED_HOSTS = []
 
 
 # Application definition
